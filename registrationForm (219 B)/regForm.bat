1fb2f73c2009ad3353cea8704204e4d0076df047
pashamelen <84251603+pashamelen@users.noreply.github.com>
2021-05-16 01:28:58 +0300

    Update regForm.bat
    Verify added

--- registrationForm (219 B)/regForm.bat
+++ registrationForm (219 B)/regForm.bat
@@ -2,8 +2,20 @@
 echo Registration form
 echo.
 set /p username=Username:
-echo %username% > username.txt
+echo %username% > yournamehere1.txt
 set /p password=Password:
-echo %password% > pass.txt
+echo %password% > yournamehere.txt
 echo Registration completed!
 echo Your username is %username%.
+echo.
+# optional
+echo Enter username again:
+set /p verify=Username: 
+if %verify%==%username% goto complete else goto error
+:complete
+cls
+echo Account %username% verifyed!
+echo.
+echo Your username and password stores in files "yournamehere1.txt" and "yournamehere.txt"
+pause
+exit
